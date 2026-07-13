75d5d6b0c60f8c031866ba5d3b484afbe85d0949
teamsolopro <team@gosolopro.com>
2026-07-12 21:37:06 -0400

    Add play, revue, opera, operetta, fringe, immersive show types

--- models/Production.js
+++ models/Production.js
@@ -26,7 +26,7 @@ const productionSchema = new mongoose.Schema(
       },
       showType: [{
         type: String,
-        enum: ['musical', 'drama', 'comedy', 'one_act', 'childrens', 'other'],
+        enum: ['musical', 'play', 'drama', 'comedy', 'one_act', 'revue', 'opera', 'operetta', 'fringe', 'immersive', 'childrens', 'other'],
       }],
       familyRating: { type: String, enum: ['G', 'PG', 'PG-13'] },
       posterImageUrl: String,
