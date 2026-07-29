2512171690776c4e2e2cbdb86d78164ccb02e723
Chris Laning <chrislaningstudio@Chriss-Mac-Studio.local>
2026-07-29 08:49:15 -0400

    Made show logos clickable on the shows page

--- cdn/assets/js/shows.js
+++ cdn/assets/js/shows.js
@@ -89,7 +89,9 @@
 
     return [
       '<div class="nsc-card">',
+      '<a href="/shows/detail?id=' + esc(p._id) + '">',
         poster,
+      '</a>',
         '<span class="nsc-card-label">' + esc(showType) + rating + '</span>',
         '<h3 class="nsc-card-title">' + title + '<span class="nsc-card-author">' + author + '</span></h3>',
         company  ? '<p class="nsc-card-company">' + esc(company) + '</p>' : '',
